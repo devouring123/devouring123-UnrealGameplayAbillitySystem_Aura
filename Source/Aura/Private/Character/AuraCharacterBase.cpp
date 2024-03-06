9ac0b72aeb7bfeb4420b5cd658f77b48ef3733c2
SMG <devouring123@gmail.com>
2024-03-06 14:15:42 +0900

    6. Player and Enemy Characters

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,16 @@
+// Copyright Sosil202403
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
---
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -0,0 +1,5 @@
+// Copyright Sosil202403
+
+
+#include "Character/AuraEnemy.h"
+
