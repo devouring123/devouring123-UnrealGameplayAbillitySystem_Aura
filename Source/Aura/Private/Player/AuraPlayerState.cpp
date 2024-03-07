818bc27a94faf993188416dd59c89f3a9d10b1bf
SMG <devouring123@gmail.com>
2024-03-07 12:06:23 +0900

    18. The Player State

---
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,9 @@
+// Copyright Sosil202403
+
+
+#include "Player/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	NetUpdateFrequency = 100.f;
+}
---
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -0,0 +1,18 @@
+// Copyright Sosil202403
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "AuraPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+public:
+	AAuraPlayerState();
+};
