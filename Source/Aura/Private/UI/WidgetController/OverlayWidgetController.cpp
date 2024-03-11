3dff9ee04f3482c9d534bd766c324530b24b6418
SMG <devouring123@gmail.com>
2024-03-11 15:44:34 +0900

    34. Broadcasting Initial Values

--- Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -3,3 +3,14 @@
 
 #include "UI/WidgetController/OverlayWidgetController.h"
 
+#include "AbilitySystem/AuraAttributeSet.h"
+
+void UOverlayWidgetController::BroadcastInitialValues()
+{
+	Super::BroadcastInitialValues();
+	
+	const UAuraAttributeSet* AuraAttributeSet = CastChecked<UAuraAttributeSet>(AttributeSet);
+	
+	OnHealthChange.Broadcast(AuraAttributeSet->GetHealth());
+	OnMaxHealthChange.Broadcast(AuraAttributeSet->GetMaxHealth());
+}
