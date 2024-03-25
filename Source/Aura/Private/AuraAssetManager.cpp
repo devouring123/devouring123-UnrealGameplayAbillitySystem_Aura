03f7b1738fffe026309bfa12fa04c9b987d10337
SMG <devouring123@gmail.com>
2024-03-25 18:49:38 +0900

    87. Aura Asset Manager

---
+++ Source/Aura/Private/AuraAssetManager.cpp
@@ -0,0 +1,21 @@
+// Copyright Sosil202403
+
+
+#include "AuraAssetManager.h"
+
+#include "AuraGameplayTags.h"
+
+UAuraAssetManager& UAuraAssetManager::Get()
+{
+	check(GEngine);
+	
+	UAuraAssetManager* AuraAssetManager = Cast<UAuraAssetManager>(GEngine->AssetManager);
+	return *AuraAssetManager;
+}
+
+void UAuraAssetManager::StartInitialLoading()
+{
+	Super::StartInitialLoading();
+	FAuraGameplayTags::InitializeNativeGameplayTags();
+	
+}
