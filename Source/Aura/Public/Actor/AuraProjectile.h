0138ab9a92a9f4322ea8129e540e3ca0e1fadbcb
SMG <devouring123@gmail.com>
2024-04-15 07:17:18 +0900

    109. Aura Projectile

---
+++ Source/Aura/Public/Actor/AuraProjectile.h
@@ -0,0 +1,32 @@
+// Copyright Sosil202403
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AuraProjectile.generated.h"
+
+class UProjectileMovementComponent;
+class USphereComponent;
+
+UCLASS()
+class AURA_API AAuraProjectile : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AAuraProjectile();
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UProjectileMovementComponent> ProjectileMovement;
+protected:
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	                     int32 OtherBodyIndex, bool bFromSweep, const FHitResult& HitResult);
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USphereComponent> Sphere;
+};
